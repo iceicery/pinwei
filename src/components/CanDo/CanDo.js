856698d6b424010f31c915e3278a31dd15cc0973
pinwei wu <pw.amy.wu@gmail.com>
2021-01-09 10:15:37 -0600

    adjust cando elements

--- src/components/CanDo/CanDo.js
+++ src/components/CanDo/CanDo.js
@@ -11,27 +11,27 @@ export default function CanDo() {
         <section className="cando">
             <ul className="cando__box">
                 <li className="cando__item">
-                    <FontAwesomeIcon icon={['fab', 'js-square']} size="5x" />
+                    <FontAwesomeIcon icon={['fab', 'js-square']} size="5x" className="cando__img" />
                     <p className="cando__text">Java Script</p>
                 </li>
                 <li className="cando__item">
-                    <FontAwesomeIcon icon={['fab', 'css3']} size="5x" />
+                    <FontAwesomeIcon icon={['fab', 'css3']} size="5x" className="cando__img" />
                     <p className="cando__text">CSS</p>
                 </li>
                 <li className="cando__item">
-                    <FontAwesomeIcon icon={['fab', 'html5']} size="5x" />
+                    <FontAwesomeIcon icon={['fab', 'html5']} size="5x" className="cando__img" />
                     <p className="cando__text">HTML5</p>
                 </li>
                 <li className="cando__item">
-                    <FontAwesomeIcon icon={['fab', 'react']} size="5x" />
+                    <FontAwesomeIcon icon={['fab', 'react']} size="5x" className="cando__img" />
                     <p className="cando__text">React</p>
                 </li>
                 <li className="cando__item">
-                    <FontAwesomeIcon icon={['fab', 'node']} size="5x" />
+                    <FontAwesomeIcon icon={['fab', 'node']} size="5x" className="cando__img" />
                     <p className="cando__text">Node.js</p>
                 </li>
                 <li className="cando__item">
-                    <FontAwesomeIcon icon={['fab', 'git-alt']} size="5x" />
+                    <FontAwesomeIcon icon={['fab', 'git-alt']} size="5x" className="cando__img" />
                     <p className="cando__text">Git</p>
                 </li>
                 <li className="cando__item">
@@ -39,7 +39,7 @@ export default function CanDo() {
                     <p className="cando__text">SAS</p>
                 </li>
                 <li className="cando__item">
-                    <FontAwesomeIcon icon={['fab', 'r-project']} size="5x" />
+                    <FontAwesomeIcon icon={['fab', 'r-project']} size="5x" className="cando__img" />
                     <p className="cando__text">R</p>
                 </li>
             </ul>
