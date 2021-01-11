141134e690f925123f0ae9369c87ffee2e36849f
pinwei wu <pw.amy.wu@gmail.com>
2021-01-10 23:02:57 -0600

    adjust css layout for different screen size/menu,project,header

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,17 +3,17 @@ import './Menu.css';
 export default function Menu() {
     return (
         <header className="menu">
-            <div className="menu__background">
-                <div className="menu__text-box">
-                    <h2 className="menu__title-hi">Hello World!</h2>
-                    <h2 className="menu__title">I am Pinwei Wu.</h2>
-                    <ul className="menu__subtitle-box">
-                        <li className="menu__subtitle">Web Developer ,</li>
-                        <li className="menu__subtitle">Engineer ,</li>
-                        <li className="menu__subtitle">Statistic Specialist ,</li>
-                        <li className="menu__subtitle">Physics Background</li>
-                    </ul>
-                </div>
+
+            <div className="menu__text-box">
+                <h2 className="menu__title-hi">Hello World!</h2>
+                <h2 className="menu__title">I am Pinwei Wu.</h2>
+                <ul className="menu__subtitle-box">
+                    <li className="menu__subtitle">Web Developer ,</li>
+                    <li className="menu__subtitle">Engineer ,</li>
+                    <li className="menu__subtitle">Statistic Specialist ,</li>
+                    <li className="menu__subtitle">Physics Background</li>
+                </ul>
+
             </div>
         </header >
     )
